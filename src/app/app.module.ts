3809a38c399563a4c0520784d15691f2b8bee66a
okomaro <okomaro@gmail.com>
2018-11-04 20:33:09 +0100

    initiak

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,45 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { WorksComponent } from './works/works.component';
+import { CvComponent } from './cv/cv.component';
+import { ContactComponent } from './contact/contact.component';
+import { ProjectComponent } from './project/project.component';
+import { SocialComponent } from './social/social.component';
+
+const appRoutes: Routes = [
+ // sets home page (base URL)
+  { path: 'home', component: HomeComponent },
+// redirects to home
+  { path: '',   redirectTo: '/home', pathMatch: 'full' },
+  { path: 'works', component: WorksComponent },
+  { path: 'cv', component: CvComponent }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    WorksComponent,
+    CvComponent,
+    ContactComponent,
+    ProjectComponent,
+    SocialComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes),
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
