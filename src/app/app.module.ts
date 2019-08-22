3b260aaaff14d6d83fc68847ca342f10aabc06ea
okomaro <okomaro@gmail.com>
2019-08-22 14:09:26 +0200

    change places

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { RouterModule, Routes } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { HttpModule }      from '@angular/http';
 import { HashLocationStrategy, LocationStrategy } from '@angular/common';
-
 import { AppComponent } from './app.component';
-
+import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import { WorksComponent } from './works/works.component';
 import { CvComponent } from './cv/cv.component';
 import { ContactComponent } from './contact/contact.component';
 import { SocialComponent } from './social/social.component';
-import { HeaderComponent } from './header/header.component';
-
-
 
 
 const appRoutes: Routes = [
@@ -32,13 +27,13 @@ const appRoutes: Routes = [
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
     FooterComponent,
     HomeComponent,
     WorksComponent,
     CvComponent,
     ContactComponent,
-    SocialComponent,
-    HeaderComponent
+    SocialComponent
   ],
   imports: [
     BrowserModule,
